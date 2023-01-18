ba3660092abbdd0e311939fbdeb8b20ece988bac
kamszeliga <kamrynszeliga@gmail.com>
2023-01-18 15:07:30 -0500

    fin

--- js/site.js
+++ js/site.js
@@ -1,11 +1,31 @@
-function displayMessage() {
-    let msg = document.getElementById('message').value;
+//Entry point AKA Controller
+function getValues() {
+    let inputString = document.getElementById('userString').value;
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'Backtracker',
-            text: msg,
-        }
-    );
+    let reversedString = reverseAString(inputString);
+
+    displayString(reversedString);
+
+}
+
+//Logic Function
+//Reverse the String
+function reverseAString(userString) {
+    let revString = '';
+
+    // reverse the string
+    for(let i = userString.length - 1; i >= 0; i = i - 1) { // starting value, condition 
+       revString += userString[i];
+    }
+
+    return revString;
+
+}
+
+//View Function
+function displayString(revString) {
+
+    document.getElementById('results').textContent = revString;
+    document.getElementById('alert').classList.remove('invisible');
+
 }
